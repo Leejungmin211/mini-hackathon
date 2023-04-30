e44ce817a6e791638811b4f929ceaa9795c893b3
Leejungmin211 <jungmin910211@gmail.com>
2023-04-30 21:37:42 +0900

    fix(Nav): 수정 Nav 위치 변경

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import { Suspense, lazy } from 'react';
 import './App.css';
+import ErrorPage from "./pages/ErrorPage";
+import styled from "styled-components"
 
 const MainCalendar = lazy(() => import('./pages/MainCalendar'));
 const TodoList = lazy(() => import("./pages/TodoList"));
@@ -10,6 +12,13 @@ const TopNav = lazy(() => import('./component/TopNav'));
 const Weather = lazy(() => import('./component/Weather'));
 const BottomNav = lazy(() => import('./component/BottomNav'));
 
+const MainWrapper = styled.div`
+display: flex;
+`
+const SideWrapper = styled.div`
+display: flex;
+flex-direction: column;
+`
 function App() {
   return (
     <BrowserRouter>
@@ -18,6 +27,10 @@ function App() {
       <div className="weather">
         <Weather />
       </div>
+     <MainWrapper>
+     <SideWrapper>
+      <BottomNav />
+      </SideWrapper>
     <div className="App">
       <TopNav />
       <main className="main">
@@ -27,9 +40,9 @@ function App() {
         <Route path="/userpage" element={<UserPage />} />
         </Routes>
       </main>
-      <BottomNav />
     </div>
-    </div>
+    </MainWrapper>
+     </div>
     </Suspense>
     </BrowserRouter>
   );
--- src/component/BottomNav.js
+++ src/component/BottomNav.js
@@ -1,50 +1,48 @@
-import { useState } from "react";
 import { BiHomeHeart } from 'react-icons/bi';
 import { BiCalendarHeart } from 'react-icons/bi';
 import { BiUserCircle } from 'react-icons/bi';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components"
 
 const Nav = styled.nav`
+  height: 250px;
   display: flex;
+  flex-direction: column;
   justify-content: space-around;
   align-items: center;
   position: relative;
-  margin-bottom: 20px;
+  margin-right: 20px;
+`
+const NavStyle = styled(NavLink)`
+.home{
+  width: 65px;
+  height: 65px;
+  color: #AFAFAF;
+}
+.calendar{
+  width: 60px;
+  height: 60px;
+  color: #AFAFAF;
+}
+.user{
+  width: 62px;
+  height: 62px;
+  color: #AFAFAF;
+}
 
-  .home{
-    width: 65px;
-    height: 65px;
-    color: #AFAFAF;
-  }
-  .calendar{
-    width: 60px;
-    height: 60px;
-    color: #AFAFAF;
-  }
-  .user{
-    width: 62px;
-    height: 62px;
-    color: #AFAFAF;
-  }
-  .active{
-    color: #6541da;
-    transition: 1s;
-  }
 `
 export default function BottomNav(){
-  const [activeNav, setActiveNav] = useState(1)
     return(
         <Nav>
-            <Link to="/" onClick={() => setActiveNav(1)}>
-              <BiCalendarHeart className={activeNav === 1? "calendar active" : "calendar"}/>
-            </Link>
-            <Link to="/todolist" onClick={() => setActiveNav(2)}>
-              <BiHomeHeart className={activeNav === 2? "home active" : "home"} />
-            </Link>
-            <Link to="/userpage" onClick={() => setActiveNav(3)}>
-              <BiUserCircle className={activeNav === 3? "user active" : "user"} />
-            </Link>
+            <NavStyle to="/" >
+              <BiCalendarHeart className="calendar"/>
+            </NavStyle>
+            <NavStyle to="/todolist">
+              <BiHomeHeart className="home" />
+            </NavStyle>
+            <NavStyle to="/userpage">
+              <BiUserCircle className="user" />
+            </NavStyle>
         </Nav>
     )
 }
