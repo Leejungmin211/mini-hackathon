349dd4cf715c25d9272a3ed1dd8c8e06179e234a
Leejungmin211 <jungmin910211@gmail.com>
2023-05-07 23:17:27 +0900

    feat(Login, Signup page): 추가 Login, Signup page

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Section = styled.section`
+  margin-top: 20px;
+`;
+
+export default function LoginPage() {
+  return (
+    <Section>
+      <div>LoginPage</div>
+    </Section>
+  );
+}
---
+++ src/pages/SignupPage.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Section = styled.section`
+  margin-top: 20px;
+`;
+
+export default function SignupPage() {
+  return (
+    <Section>
+      <div>SignupPage</div>
+    </Section>
+  );
+}
